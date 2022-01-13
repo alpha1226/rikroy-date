0889f499b9527eabb9935ea55d651a54aa3b5f08
gouyoungwoo <rnduddn1226@gmail.com>
2022-01-13 14:15:47 +0900

    버그 수정

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ module.exports = {
    * @summary get date(YYYYMMDD) as full string(8)
    * @author rikroy
    * @since 2022.01.03
-   * @returns YYYYMMDD
+   * @returns {string(8)} YYYYMMDD
    */
   getNowDateString() {
     let now = new Date();
     let dateString =
       now.getFullYear() +
       '' +
-      (now.getMonth() >= 9 ? now.getMonth() + 1 : '0' + now.getMonth() + 1) +
+      (now.getMonth() >= 9 ? (now.getMonth() + 1) : '0' + (now.getMonth() + 1)) +
       '' +
       (now.getDate() >= 10 ? now.getDate() : '0' + now.getDate());
     return dateString;
@@ -22,14 +22,14 @@ module.exports = {
    * @summary get datetime(YYYYMMDDHHMMSS) as full string(14)
    * @author rikroy
    * @since 2022.01.03
-   * @returns YYYYMMDDHHMMSS
+   * @returns {string(14) YYYYMMDDHHMMSS
    */
   getNowDateTimeString() {
     let now = new Date();
     let dateString =
       now.getFullYear() +
       '' +
-      (now.getMonth() >= 9 ? now.getMonth() + 1 : '0' + now.getMonth() + 1) +
+      (now.getMonth() >= 9 ? now.getMonth() + 1 : '0' + (now.getMonth() + 1)) +
       '' +
       (now.getDate() >= 10 ? now.getDate() : '0' + now.getDate()) +
       (now.getHours() >= 10 ? now.getHours() : '0' + now.getHours()) +
@@ -44,7 +44,7 @@ module.exports = {
    * @author rikroy
    * @since 2022.01.03
    * @param {Date} date
-   * @returns YYYYMMDD
+   * @returns {string(8)} YYYYMMDD
    * @throws unvalid date object
    */
   getDateString(date) {
@@ -52,7 +52,7 @@ module.exports = {
       let dateString =
       date.getFullYear() +
       '' +
-      (date.getMonth() >= 9 ? date.getMonth() + 1 : '0' + date.getMonth() + 1) +
+      (date.getMonth() >= 9 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1)) +
       '' +
       (date.getDate() >= 10 ? date.getDate() : '0' + date.getDate());
       return dateString;
@@ -66,14 +66,14 @@ module.exports = {
    * @author rikroy
    * @since 2022.01.03
    * @param {Date} date 
-   * @returns YYYYMMDD
+   * @returns {string(14)} YYYYMMDDHHMMSS
    * @throws unvalid date object
    */
   getDateTimeString(date) {
     if(date instanceof Date){
       let dateString =
         date.getFullYear() + '' +
-        (date.getMonth() >= 9 ? date.getMonth() + 1 : '0' + date.getMonth() + 1) + '' +
+        (date.getMonth() >= 9 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1)) + '' +
         (date.getDate() >= 10 ? date.getDate() : '0' + date.getDate()) +
         (date.getHours() >= 10 ? date.getHours() : '0' + date.getHours()) +
         (date.getMinutes() >= 10 ? date.getMinutes() : '0' + date.getMinutes()) +
